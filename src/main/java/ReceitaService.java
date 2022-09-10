690d6dbe5333cb1b36e8d3dd83087c55121e2f0b
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2022-09-09 22:50:21 -0300

    adicionando documentacao nos metodos da classe ReceitaService

--- src/main/java/ReceitaService.java
+++ src/main/java/ReceitaService.java
@@ -11,13 +11,21 @@ import java.util.Scanner;
  */
 public class ReceitaService {
 
-	// Esta é a implementação interna do "servico" do banco central.
-    // Veja o código fonte abaixo para ver os formatos esperados pelo Banco Central neste cenário.
-
+    /**
+     * Esta é a implementação interna do "servico" do banco central.
+     * Veja o código fonte abaixo para ver os formatos esperados pelo Banco Central neste cenário.
+     * @param agencia variável string que receberá o número da agencia.
+     * @param conta variável string que receberá o número da conta.
+     * @param saldo variável string que receberá o saldo.
+     * @param status variável string que receberá o status.
+     * @return true, se os dados passados satisfazerem todas as condições;
+     *         false, se alguma das condições não forem satisfeitas.
+     * @throws RuntimeException
+     * @throws InterruptedException
+     */
     public boolean atualizarConta(String agencia, String conta, double saldo, String status)
             throws RuntimeException, InterruptedException {
-		
-			
+
         // Formato agencia: 0000
         if (agencia == null || agencia.length() != 4) {
             return false;
@@ -52,6 +60,14 @@ public class ReceitaService {
         return true;
     }
 
+    /**
+     * Função responsável pela leitura, processamento e exportação do arquivo .csv
+     * com o resultado do processamento realizado.
+     * @author Francivaldo Napoleão
+     * @since 09/09/2022
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public void importarArquivoSicredi() throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("saida-processamento.csv"));
@@ -88,10 +104,37 @@ public class ReceitaService {
         scanner.close();
     }
 
+    /**
+     * Função auxiliar utilizada para formatar a conta bancária,
+     * adicionando o hífen (-) entre o penúltimo e último número.
+     * @author Francivaldo Napoleão
+     * @since 09/09/2022
+     * @param contaSemSeparador - variável string que receberá o número da conta
+     *                            sem o hífen (-).
+     * @return string com o número da conta formatado no padrão estipulado.
+     */
     private String formatarConta(String contaSemSeparador){
         return contaSemSeparador.substring(0,contaSemSeparador.length()-1)+"-"+contaSemSeparador.substring(contaSemSeparador.length()-1);
     }
 
+    /**
+     * Função auxiliar utilizada para formatar os dados para o padrão do arquivo
+     * .csv de saída.
+     * @author Francivaldo Napoleão
+     * @since 09/09/2022
+     * @param conta     - variável string com a informação da conta bancária lida no
+     *                    arquivo .csv de entrada;
+     * @param agencia   - variável string com a informação da agência bancária lida no
+     *                    arquivo .csv de entrada;
+     * @param saldo     - variável double com a informação do saldo lido no
+     *                    arquivo .csv de entrada;
+     * @param status    - variável string com a informação do status lido no
+     *                    arquivo .csv de entrada;
+     * @param retornoProcessamento - variável string com o resultado do processamento realizado pelo
+     *                               método atualizaConta().
+     *
+     * @return string com os dados lidos e formatados no padrão estipulado.
+     */
     private String formatarLinhaParaCsv(String conta, String agencia, double saldo, String status, boolean retornoProcessamento){
         return String.format(Locale.GERMANY,"%s;%s;%.2f;%s;%s", agencia, conta, saldo, status, retornoProcessamento);
     }
